6db95104ada5646cb39421dd56371774b2c28287
Bill Newman <billenewman4@gmail.com>
2023-06-23 13:35:56 +0200

    Updated server link in react app and used gcloud secrets in server

--- mern/server/server.js
+++ mern/server/server.js
@@ -2,52 +2,75 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-require('dotenv').config();
+const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-// Listen to the App Engine-specified port, or 8080 otherwise
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
 
-const contactEmail = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_PASS,
-    },
-  });
-  
-  contactEmail.verify((error) => {
-    if (error) {
-      console.log(error);
-      console.log(contactEmail);
-    } else {
-      console.log("Ready to Send");
-    }
+const client = new SecretManagerServiceClient();
+
+let gmailUser;
+let gmailPass;
+
+async function accessSecretVersion(secretName) {
+  const [version] = await client.accessSecretVersion({
+    name: `projects/personal-website-server-390615/secrets/${secretName}/versions/latest`,
   });
 
-  router.post("/contactServer", (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message; 
-    const mail = {
-      from: name,
-      to: "15237bn@gmail.com",
-      subject: "Contact Form Submission",
-      html: `<p>Name: ${name}</p>
-             <p>Email: ${email}</p>
-             <p>Message: ${message}</p>`,
-    };
-    contactEmail.sendMail(mail, (error) => {
+  const payload = version.payload.data.toString('utf8');
+
+  return payload;
+}
+
+Promise.all([accessSecretVersion('Gmail'), accessSecretVersion('gmail_password')])
+  .then(([user, pass]) => {
+    gmailUser = user;
+    gmailPass = pass;
+
+    const contactEmail = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: gmailUser,
+        pass: gmailPass,
+      },
+    });
+
+    contactEmail.verify((error) => {
       if (error) {
-        res.json({ status: "ERROR" });
+        console.log(error);
+        console.log(contactEmail);
       } else {
-        res.json({ status: "Message Sent" });
+        console.log("Ready to Send");
       }
     });
-  });
+
+    router.post("/contactServer", (req, res) => {
+      const name = req.body.name;
+      const email = req.body.email;
+      const message = req.body.message; 
+      const mail = {
+        from: name,
+        to: "15237bn@gmail.com",
+        subject: "Contact Form Submission",
+        html: `<p>Name: ${name}</p>
+              <p>Email: ${email}</p>
+              <p>Message: ${message}</p>`,
+      };
+      contactEmail.sendMail(mail, (error) => {
+        if (error) {
+          res.json({ status: "ERROR" });
+        } else {
+          res.json({ status: "Message Sent" });
+        }
+      });
+    });
+
+  })
+  .catch(console.error);
